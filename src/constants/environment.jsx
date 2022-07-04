c221bac2fd9e725fd1a40e98521cc121b6596344
Santiago <santisemhan2@gmail.com>
2022-07-03 23:53:27 -0300

    Azure environment

--- src/constants/environment.jsx
+++ src/constants/environment.jsx
@@ -1,5 +1,5 @@
 const environment = {
-    API_URL: "https://e956-2800-40-1a-4ef-692b-e465-1030-10ef.sa.ngrok.io/api"
+    API_URL: "https://recipesuadeapi.azurewebsites.net/api"
 }
 
 export default environment;
